f38d600858d03378644efcb8a2c42bb75d224c60
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-11-13 14:58:16 +0000

    Add Splash Screen

---
+++ Kourse/app/src/main/java/com/kourseco/kourse/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.kourseco.kourse
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kourseco.kourse.databinding.ActivitySplashBinding
+import kotlin.concurrent.thread
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        thread {
+            Thread.sleep((3 * 1000).toLong())
+            startMainActivity()
+        }.priority = Thread.NORM_PRIORITY
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
