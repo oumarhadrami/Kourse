c76d992d784406cb1f0278a001b473c426e2577c
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-11-13 18:48:09 +0000

    Create UI for intro fragment Create ViewPager to swipe 3 layouts

--- Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
@@ -6,20 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.kourseco.kourse.R
+import com.kourseco.kourse.databinding.FragmentIntroBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class IntroFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding : FragmentIntroBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro,container,false)
+
+        val layoutAdapter = LayoutPagerAdapter(layoutInflater)
+        binding.introViewpager.adapter = layoutAdapter
+        return binding.root
     }
 
 
