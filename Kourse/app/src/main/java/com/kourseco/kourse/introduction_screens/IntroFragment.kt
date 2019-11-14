18c410b07ed1600bd240a5d0d303e35257032294
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-11-14 13:38:55 +0000

    Add TabLayout dots, various colors, strings, and margins, to the intro fragment. Add Login Button with different substring color

--- Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
@@ -1,7 +1,11 @@
 package com.kourseco.kourse.introduction_screens
 
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,12 +20,30 @@ import com.kourseco.kourse.databinding.FragmentIntroBinding
  */
 class IntroFragment : Fragment() {
 
+    private lateinit var binding : FragmentIntroBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding : FragmentIntroBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro,container,false)
-
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro,container,false)
         val layoutAdapter = LayoutPagerAdapter(layoutInflater)
         binding.introViewpager.adapter = layoutAdapter
+        binding.tabLayout.setupWithViewPager(binding.introViewpager)
+
+
+        //code to make the Login part of the text orange
+        makeLoginOrange()
+
+
+
+
         return binding.root
+    }
+
+    private fun makeLoginOrange() {
+        val text = "Have an account? Login"
+        val ss : SpannableString = SpannableString(text)
+        val orangeLogin : ForegroundColorSpan = ForegroundColorSpan(Color.parseColor("#fc8019"))
+        ss.setSpan(orangeLogin, 17,22, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.loginButton.text = ss
+
     }
 
 
