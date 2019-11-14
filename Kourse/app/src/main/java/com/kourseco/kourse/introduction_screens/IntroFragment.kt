cf5710d9b4accf788d755850648f8d6a2cb2623b
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-11-14 15:44:47 +0000

    Add orange dot for tabLayout Add Navigation to LogIn page

--- Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/introduction_screens/IntroFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 
 import com.kourseco.kourse.R
 import com.kourseco.kourse.databinding.FragmentIntroBinding
@@ -23,15 +24,20 @@ class IntroFragment : Fragment() {
     private lateinit var binding : FragmentIntroBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro,container,false)
+
+        // setting up adapter and tabLayout with viewPager
         val layoutAdapter = LayoutPagerAdapter(layoutInflater)
         binding.introViewpager.adapter = layoutAdapter
         binding.tabLayout.setupWithViewPager(binding.introViewpager)
 
 
-        //code to make the Login part of the text orange
+        //Make the Login part of the text orange
         makeLoginOrange()
 
-
+        // Naviagte to LogIn page
+        binding.loginButton.setOnClickListener {
+            it.findNavController().navigate(IntroFragmentDirections.actionIntroFragmentToLogInFragment())
+        }
 
 
         return binding.root
