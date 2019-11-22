852ecd377eb6dead5c0448af5487bcd25be6d828
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-11-22 14:49:34 +0000

    add preps for phone number verification

--- Kourse/app/src/main/java/com/kourseco/kourse/HomeActivity.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/HomeActivity.kt
@@ -1,22 +1,34 @@
 package com.kourseco.kourse
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.kourseco.kourse.introduction_screen.MainActivity
 
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
 
+        // Initialize firebaseAuth
+
         fab.setOnClickListener { view ->
+            auth.signOut()
+            startActivity(Intent(applicationContext,MainActivity::class.java))
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+
 
 }
--- Kourse/app/src/main/java/com/kourseco/kourse/SplashActivity.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/SplashActivity.kt
@@ -4,21 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.kourseco.kourse.introduction_screen.MainActivity
 import com.kourseco.kourse.databinding.ActivitySplashBinding
 import kotlin.concurrent.thread
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
 
     private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
 
+        // initialize firebase
+        auth = FirebaseAuth.getInstance()
+
         thread {
             Thread.sleep((3 * 1000).toLong())
-            startMainActivity()
+            if (auth.currentUser == null)
+                startMainActivity()
+            else
+                startHomeActivity()
         }.priority = Thread.NORM_PRIORITY
+    }
+
+    private fun startHomeActivity() {
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun startMainActivity() {
