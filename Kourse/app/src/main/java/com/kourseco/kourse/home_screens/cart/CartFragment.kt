86ad4fb43abc4859d0323c1fd6b55e4be89d9961
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-12-01 13:04:13 +0000

    Change Resend button design Add permissions for network access

---
+++ Kourse/app/src/main/java/com/kourseco/kourse/home_screens/cart/CartFragment.kt
@@ -0,0 +1,31 @@
+package com.kourseco.kourse.home_screens.cart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.kourseco.kourse.R
+
+class CartFragment : Fragment() {
+
+    private lateinit var cartViewModel: CartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        cartViewModel =
+            ViewModelProviders.of(this).get(CartViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        cartViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
