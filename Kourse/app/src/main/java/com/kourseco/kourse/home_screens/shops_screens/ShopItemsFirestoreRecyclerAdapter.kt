a9c2ca047f967d3d86cc3af9e1fab1b0aeceda13
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-12-19 21:54:00 +0000

    Finish itemCount logic

--- Kourse/app/src/main/java/com/kourseco/kourse/home_screens/shops_screens/ShopItemsFirestoreRecyclerAdapter.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/home_screens/shops_screens/ShopItemsFirestoreRecyclerAdapter.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.protobuf.StringValue
 import com.kourseco.kourse.R
 import com.kourseco.kourse.databinding.ShopitemItemBinding
 
@@ -27,8 +28,8 @@ class ShopItemsFirestoreRecyclerAdapter(options: FirestoreRecyclerOptions<ShopIt
     inner class ViewHolder  constructor(val binding: ShopitemItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-
         fun bind(item: ShopItem) {
+            binding.itemCount.text = item.shopItemCount.toString()
             binding.shopItemName.text = item.shopItemName
             binding.shopItemPrice.text = item.shopItemPrice
             Glide.with(binding.shopItemImage.context)
@@ -39,7 +40,31 @@ class ShopItemsFirestoreRecyclerAdapter(options: FirestoreRecyclerOptions<ShopIt
                         .error(R.drawable.ic_broken_image)
                 )
                 .into(binding.shopItemImage)
+            binding.add.setOnClickListener {
+                item.shopItemCount = item.shopItemCount.plus(1)
+                binding.itemCount.text = item.shopItemCount.toString()
+                binding.add.visibility = View.GONE
+                binding.incdecContainer.visibility = View.VISIBLE
+            }
 
+            binding.addMore.setOnClickListener {
+                if (item.shopItemCount < 10)
+                item.shopItemCount = item.shopItemCount.plus(1)
+                binding.itemCount.text = item.shopItemCount.toString()
+            }
+
+            binding.removeMore.setOnClickListener {
+                if (item.shopItemCount >= 1) {
+                    binding.add.visibility = View.GONE
+                    item.shopItemCount = item.shopItemCount.minus(1)
+                    binding.itemCount.text = item.shopItemCount.toString()
+                }
+                if(item.shopItemCount == 0) {
+                    binding.itemCount.text = item.shopItemCount.toString()
+                    binding.incdecContainer.visibility = View.GONE
+                    binding.add.visibility = View.VISIBLE
+                }
+            }
 
             binding.executePendingBindings()
         }
