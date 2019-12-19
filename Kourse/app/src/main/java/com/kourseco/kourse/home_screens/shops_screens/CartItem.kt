4cf6ab938e877a1b3e3663ec36040e6b8ef6661f
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2019-12-19 23:06:18 +0000

    Add Cart Design

--- Kourse/app/src/main/java/com/kourseco/kourse/home_screens/shops_screens/CartItem.kt
+++ Kourse/app/src/main/java/com/kourseco/kourse/home_screens/shops_screens/CartItem.kt
@@ -1,3 +1,4 @@
 package com.kourseco.kourse.home_screens.shops_screens
 
-data class CartItem(var product: ShopItem, var quantity: Int = 0)
+data class CartItem(var shopItem: ShopItem,
+                    var quantity: Int = 0)
